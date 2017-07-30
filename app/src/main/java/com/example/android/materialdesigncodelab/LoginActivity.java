dc93c52894d16a54aa542b41cc6785d60c723ea1
Nico Moccagatta <nicomoccagatta@gmail.com>
2017-07-30 18:39:13 -0300

    Log in flow WORKING

--- app/src/main/java/com/example/android/materialdesigncodelab/LoginActivity.java
+++ app/src/main/java/com/example/android/materialdesigncodelab/LoginActivity.java
@@ -422,6 +422,9 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
                             Log.d(TAG, "signInWithCredential:onComplete:" + task.isSuccessful());
                             Toast.makeText(LoginActivity.this, "Authentication SUCCESS!!!.",
                                     Toast.LENGTH_SHORT).show();
+                            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                            startActivity(intent);
+                            finish();
                         }
                         // ...
                     }
