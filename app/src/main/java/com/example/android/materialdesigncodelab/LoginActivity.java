9a59981509fa1706c7a6dd658cc4a03729c79f7e
Nico Moccagatta <nicomoccagatta@gmail.com>
2017-07-31 23:40:31 -0300

    Fix in master

--- app/src/main/java/com/example/android/materialdesigncodelab/LoginActivity.java
+++ app/src/main/java/com/example/android/materialdesigncodelab/LoginActivity.java
@@ -90,7 +90,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        setupActionBar();
+        //setupActionBar();
         // Set up the login form.
         mEmailView = (AutoCompleteTextView) findViewById(R.id.email);
         populateAutoComplete();
