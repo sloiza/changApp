b7e0c0e48d05ac330c3fb1169b19badcfe2190f9
Nico Moccagatta <nicomoccagatta@gmail.com>
2017-08-10 01:51:14 -0300

    LIST VIEW WORKING for changas in process

--- app/src/main/java/com/example/android/materialdesigncodelab/ListContentFragment.java
+++ app/src/main/java/com/example/android/materialdesigncodelab/ListContentFragment.java
@@ -25,11 +25,25 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static android.content.ContentValues.TAG;
+import static com.example.android.materialdesigncodelab.ListContentFragment.ContentAdapter.mIDS;
 
 /**
  * Provides UI for the view with List.
@@ -49,23 +63,23 @@ public class ListContentFragment extends Fragment {
     }
 
     public static class ViewHolder extends RecyclerView.ViewHolder {
-        public ImageView avator;
+        public ImageView picture;
         public TextView name;
         public TextView description;
         public ViewHolder(LayoutInflater inflater, ViewGroup parent) {
             super(inflater.inflate(R.layout.item_list, parent, false));
-            avator = (ImageView) itemView.findViewById(R.id.list_avatar);
+            picture = (ImageView) itemView.findViewById(R.id.list_avatar);
             name = (TextView) itemView.findViewById(R.id.list_title);
             description = (TextView) itemView.findViewById(R.id.list_desc);
-            itemView.setOnClickListener(new View.OnClickListener() {
+            /*itemView.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
                     Context context = v.getContext();
-                    Intent intent = new Intent(context, DetailActivity.class);
-                    intent.putExtra(DetailActivity.EXTRA_POSITION, getAdapterPosition());
+                    Intent intent = new Intent(context, DetailActivity2.class);
+                    intent.putExtra(DetailActivity.EXTRA_POSITION, mIDS[getAdapterPosition()]);
                     context.startActivity(intent);
                 }
-            });
+            });*/
         }
     }
 
@@ -73,21 +87,67 @@ public class ListContentFragment extends Fragment {
      * Adapter to display recycler view.
      */
     public static class ContentAdapter extends RecyclerView.Adapter<ViewHolder> {
-        // Set numbers of List in RecyclerView.
-        private static final int LENGTH = 18;
+        private DatabaseReference mChangasReference;
 
-        private final String[] mPlaces;
-        private final String[] mPlaceDesc;
-        private final Drawable[] mPlaceAvators;
+        public static int LENGTH = 0;
+
+        public static  String[] mIDS;
+        private static String[] mChangasTitle;
+        private static String[] mChangasDescription;
+        private static Integer[] mChangasCategory;
+        private final Drawable[] mChangasPictures;
 
         public ContentAdapter(Context context) {
+            mChangasReference = FirebaseDatabase.getInstance().getReference("changas");
+            ValueEventListener changaListener = new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+                    List<String> ids = new ArrayList<String>();
+                    List<String> titles = new ArrayList<String>();
+                    List<String> descriptions = new ArrayList<String>();
+                    List<Integer> categories = new ArrayList<Integer>();
+
+                    LENGTH = 0;
+                    for (DataSnapshot changaSnapshot: dataSnapshot.getChildren()) {
+                        Changa changa = changaSnapshot.getValue(Changa.class);
+                        if(changa.status.equals("enProceso")){
+                            String idChanguero = changaSnapshot.child("changuero").getValue(String.class);
+                            if(idChanguero.equals(FirebaseAuth.getInstance().getCurrentUser().getUid())){
+                                ids.add(changa.id);
+                                titles.add(changa.title);
+                                descriptions.add(changa.body);
+                                categories.add(changa.category);
+                                LENGTH++;
+                            }
+                        }
+                    }
+                    mIDS = new String[LENGTH];
+                    mChangasTitle = new String[LENGTH];
+                    mChangasDescription = new String[LENGTH];
+                    mChangasCategory = new Integer[LENGTH];
+
+                    ids.toArray(mIDS);
+                    titles.toArray(mChangasTitle);
+                    descriptions.toArray(mChangasDescription);
+                    categories.toArray(mChangasCategory);
+                    notifyDataSetChanged();
+                }
+
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
+                    // Getting Post failed, log a message
+                    Log.w(TAG, "loadPost:onCancelled", databaseError.toException());
+                    // ...
+                }
+            };
+            mChangasReference.addValueEventListener(changaListener);
+
+
             Resources resources = context.getResources();
-            mPlaces = resources.getStringArray(R.array.places);
-            mPlaceDesc = resources.getStringArray(R.array.place_desc);
-            TypedArray a = resources.obtainTypedArray(R.array.place_avator);
-            mPlaceAvators = new Drawable[a.length()];
-            for (int i = 0; i < mPlaceAvators.length; i++) {
-                mPlaceAvators[i] = a.getDrawable(i);
+            TypedArray a = resources.obtainTypedArray(R.array.changas_imgs);
+            mChangasPictures = new Drawable[a.length()];
+            for (int i = 0; i < mChangasPictures.length; i++) {
+                mChangasPictures[i] = a.getDrawable(i);
             }
             a.recycle();
         }
@@ -98,9 +158,10 @@ public class ListContentFragment extends Fragment {
 
         @Override
         public void onBindViewHolder(ViewHolder holder, int position) {
-            holder.avator.setImageDrawable(mPlaceAvators[position % mPlaceAvators.length]);
-            holder.name.setText(mPlaces[position % mPlaces.length]);
-            holder.description.setText(mPlaceDesc[position % mPlaceDesc.length]);
+            int nPos = LENGTH-position-1;
+            holder.picture.setImageDrawable(mChangasPictures[mChangasCategory[nPos]]);
+            holder.name.setText(mChangasTitle[nPos]);
+            holder.description.setText(mChangasDescription[nPos]);
         }
 
         @Override
